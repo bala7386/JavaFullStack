840f0f68aa45abbd3d650430c24e15e9c9e9d75e
bala7386 <gbalu339@gmail.com>
2026-08-25 16:38:50 +0530

    Add PrintStudentDetails class to display student info

---
+++ PrintStudentDetails.java
@@ -0,0 +1,22 @@
+class PrintStudentDetails
+{
+public static void main(String [] args)
+{
+String name="balu";
+int age=22;
+char gender='M';
+String degree="btech";
+String stream="cse";
+String collegename="sviet";
+int yop=2026;
+double cgpa=7.29;
+System.out.println(name);
+System.out.println(age);
+System.out.println(gender);
+System.out.println(degree);
+System.out.println(stream);
+System.out.println(collegename);
+System.out.println(yop);
+System.out.println(cgpa");
+}
+}
