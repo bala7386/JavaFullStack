6fab65a8fef6acfe99928120fc50649de4b75f2f
bala7386 <gbalu339@gmail.com>
2026-08-28 12:02:58 +0530

    Create MathFormula.java

---
+++ MathFormula.java
@@ -0,0 +1,12 @@
+class MathFormula
+{
+public static void main(String [] args)
+{
+int a=5;
+int b=6;
+int res1 = ((a+b)+(a+b));
+int res2 =((a*b)+(b*b)+(2*a*b));
+System.out.println(res1);
+System.out.println(res2);
+}
+}
