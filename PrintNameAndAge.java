c303da14bcdbaac9887d7fecc7123ba2a010bc58
bala7386 <gbalu339@gmail.com>
2026-09-04 11:59:58 +0530

    Add PrintNameAndAge class to read user input

---
+++ PrintNameAndAge.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class PrintNameAndAge
+{
+	public static void main(String [] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter your name....");
+	String name=sc.next();
+	System.out.println("enter your age....");
+	int age=sc.nextInt();
+	System.out.println(name);
+	System.out.println(age);
+}
+}
