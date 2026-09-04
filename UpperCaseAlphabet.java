d930cdffac2c3be5dbb1e131acb27fbbedae9316
bala7386 <gbalu339@gmail.com>
2026-09-04 11:58:06 +0530

    Add UppercaseAlphabet class to check uppercase letters

---
+++ UpperCaseAlphabet.java
@@ -0,0 +1,9 @@
+class UppercaseAlphabet
+{
+	public static void main(String [] args)
+{	
+	char ch='F';
+	String res=(ch>='A' && ch<='Z')? "uppercasealphabet":"not a uppercasealphabet";
+	System.out.println(res);
+}
+}
