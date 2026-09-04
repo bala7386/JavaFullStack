e0ed0d7373c87e4504ef2873749c44c0ee63579e
bala7386 <gbalu339@gmail.com>
2026-09-04 12:02:23 +0530

    Add LowercaseAlphabet class to check character case

---
+++ LowercaseAlphabet.java
@@ -0,0 +1,9 @@
+class LowercaseAlphabet
+{
+	public static void main(String [] args)
+{
+	char ch='b';
+	String res=(ch>='a' && ch<='z')? "lowercasealphabet":"not a lowercasealphabet";
+	System.out.println(res);
+}
+}
