c9dec76d0e18e3b8a66e42630acda71e34deddfe
bala7386 <gbalu339@gmail.com>
2026-09-17 21:36:59 +0530

    Add SquareTheNumber program to square integers

---
+++ SquareTheNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class SquareTheNumber
+{
+	public static void main(String [] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter m value..");
+	int m=sc.nextInt();
+	System.out.println("enter n value..");
+	int n=sc.nextInt();
+
+	int i=m;
+	while(i<=n)
+{
+	System.out.println(i*i);
+	i++;
+}
+
+
+}
+}
