1f2c548a19e8e7c41a264b3b1dead7ddaccf048c
bala7386 <gbalu339@gmail.com>
2026-09-17 21:34:39 +0530

    Add CubeOfNumber program to calculate cubes

---
+++ CubeOfNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class CubeOfNumber
+{
+	public static void main(String [] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter m value....");
+	int m=sc.nextInt();
+	System.out.println("enter n value....");
+	int n=sc.nextInt();
+	 
+	int i=m;
+	while(i<=n)
+{
+	System.out.println(i*i*i);
+	i++;
+}
+
+}
+}
