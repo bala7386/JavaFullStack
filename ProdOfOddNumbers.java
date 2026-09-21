b9a339d66fb6072894426733e7bb6f4f8a3b1852
bala7386 <gbalu339@gmail.com>
2026-09-21 19:48:20 +0530

    Add ProdOfOddNumbers class to calculate product of odds

---
+++ ProdOfOddNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class ProdOfOddNumbers
+{
+	public static void main(String [] args)
+{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the m value....");
+	int m=sc.nextInt();
+	System.out.println("enter the n value....");
+	int n=sc.nextInt();
+	int prod=1;
+	for (int i=m;i<=n;i++)
+{
+	if (i%2!=0)
+	prod=prod*i;
+}
+	System.out.println("product of odd numbers=" +prod);
+
+}
+}
